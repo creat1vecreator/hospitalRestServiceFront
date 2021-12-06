962e8581b6dcac071ee60fc4f880b36d245040e1
Андрей <andrey@MacBook-Pro.local>
2021-12-06 14:19:28 +0300

    error with styles

---
+++ src/Types/DoctorType.tsx
@@ -0,0 +1,17 @@
+
+type Doctor = {
+
+    doctorId: Number;
+
+    firstName: string;
+
+    lastName: string;
+
+    averageRate: number;
+
+    speciality: number;
+
+    isInHospital: boolean;
+
+}
+export default Doctor;
